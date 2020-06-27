2d5f32be4ce731ddad166731ba5c1c0330088b72
Erikqtx <glaser-erik@web.de>
2020-06-27 15:31:22 +0200

    Grade.css

--- student-grading/src/Grade.js
+++ student-grading/src/Grade.js
@@ -2,11 +2,25 @@ import React, { useState } from "react";
 
 function Grade() {
   let [grade, setGrade] = useState(6); //Defaultwert wo wir anfangen Array mit 2 Feldern
+  function inc() {
+    setGrade(++grade);
+  }
+  function dec() {
+    setGrade(--grade);
+  }
+  function format() {
+    if (grade >= 5) {
+      return "grade--negative";
+    } else if (grade <= 2) {
+      return "grade--positive";
+    }
+    return "grade--normal";
+  }
   return (
     <div>
-      <button>+</button>
-      <span>1</span>
-      <button>-</button>
+      <button onClick={inc}>+</button>
+      <span className={format()}>{grade}</span>
+      <button onClick={dec}>-</button>
     </div>
   );
 }
